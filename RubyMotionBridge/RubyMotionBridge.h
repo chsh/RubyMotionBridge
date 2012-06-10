86ed5dd9e940d09279083b8704558d1a3aa26544
CHIKURA Shinsaku <shinsaku@chikura.me>
2012-06-10 21:40:03 +0900

    Initial Commit

---
+++ RubyMotionBridge/RubyMotionBridge.h
@@ -0,0 +1,13 @@
+//
+//  RubyMotionBridge.h
+//  RubyMotionBridge
+//
+//  Created by CHIKURA Shinsaku on 12/06/10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RubyMotionBridge : NSObject
+
+@end
---
+++ RubyMotionBridge/RubyMotionBridge.m
@@ -0,0 +1,13 @@
+//
+//  RubyMotionBridge.m
+//  RubyMotionBridge
+//
+//  Created by CHIKURA Shinsaku on 12/06/10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "RubyMotionBridge.h"
+
+@implementation RubyMotionBridge
+
+@end
